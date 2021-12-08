2379c54e4bfe0538c9f2da531796c5f7de4719d8
tinsaebelay <tinsaedev@gmail.com>
2021-12-08 08:18:55 +0300

    Theme and Layout Defined

---
+++ sazx_wizard/scripts/wizard.js
@@ -0,0 +1,11 @@
+
+
+/**
+ * 
+ * @param {*} config The JSON used to construct the wizard
+ * @param {*} rootDom  The Id of the div in which the prepared wizard is injected to
+ */
+ let initWizard = function( config, rootDom ){
+     
+    console.log("Initializing Wizard");
+}
